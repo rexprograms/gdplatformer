4ca336708054127252b80caf1aa1cdcf23b09c85
Rex J <emulsifier@gmail.com>
2023-03-26 20:37:32 -0600

    fix coin collision, add idle animation, add pickup animation

--- scenes/Coin.gd
+++ scenes/Coin.gd
@@ -1,17 +1,10 @@
-extends Area2D
+extends Node2D
 
 export var value=1
 
 signal coin_picked_up(value)
 
-
-func _on_Coin_area_entered(area):
-	print("coin picked up")
-	if visible:
-		hide()
-		$RespawnTimer.start()
-		emit_signal("coin_picked_up", value)
-
-
-func _on_RespawnTimer_timeout():
-	show()
+func _on_CollisionArea_area_entered(area):
+	$CollisionArea/Shape.disabled = true
+	$AnimationPlayer.play("Pickup")
+	emit_signal("coin_picked_up", value)
--- scenes/World.gd
+++ scenes/World.gd
@@ -6,9 +6,8 @@ func ready():
 	update_coins()
 
 func _on_Coin_coin_picked_up(value):
-	print("coin")
 	coins+=value
 	update_coins()
 	
 func update_coins():
-	$HUD/Coins.text = "%d" % coins
+	$HUD/CoinLabel.text = "%d" % coins
